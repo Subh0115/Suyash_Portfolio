cd878609c0854c0d210d8abbaf6264d68f2fa487
Suyash Ahire <suyashahire@example.com>
2025-07-27 20:37:46 +0530

    style: make project details popup smaller for better UX

--- src/components/ProjectDetails.jsx
+++ src/components/ProjectDetails.jsx
@@ -11,7 +11,7 @@ const ProjectDetails = ({
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center w-full h-full overflow-hidden backdrop-blur-sm">
       <motion.div
-        className="relative max-w-2xl border shadow-sm rounded-2xl bg-gradient-to-l from-midnight to-navy border-white/10"
+        className="relative max-w-md border shadow-sm rounded-2xl bg-gradient-to-l from-midnight to-navy border-white/10"
         initial={{ opacity: 0, scale: 0.5 }}
         animate={{ opacity: 1, scale: 1 }}
       >
@@ -22,7 +22,7 @@ const ProjectDetails = ({
           <img src="assets/close.svg" className="w-6 h-6" />
         </button>
         <img src={image} alt={title} className="w-full rounded-t-2xl" />
-        <div className="p-5">
+        <div className="p-3 sm:p-5">
           <h5 className="mb-2 text-2xl font-bold text-white">{title}</h5>
           <p className="mb-3 font-normal text-neutral-400">{description}</p>
           {subDescription.map((subDesc, index) => (
